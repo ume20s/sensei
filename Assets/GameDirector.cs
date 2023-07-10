adb85119f3cff9ff2ddd7770b735869a91d343df
ume20s <ume20s@gmail.com>
2023-07-10 22:23:51 +0900

    first commit

--- Assets/GameDirector.cs
+++ Assets/GameDirector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameDirector : MonoBehaviour
 {
     // ゲームオブジェクト
+    public GameObject blockPrefab;
     GameObject[] block = new GameObject[50];            // ブロック
 
     // ブロックプレファブのスプライトレンダラーコンポーネント
@@ -17,7 +18,12 @@ public class GameDirector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        for (int i = 0; i<49; i++)
+        {
+            block[i] = Instantiate(blockPrefab, new Vector3(dt.x[i], dt.y[i], 0.0f), Quaternion.identity);
+            spriteRenderer = block[i].GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = senseiSprite[i];
+        }
     }
 
     // Update is called once per frame
